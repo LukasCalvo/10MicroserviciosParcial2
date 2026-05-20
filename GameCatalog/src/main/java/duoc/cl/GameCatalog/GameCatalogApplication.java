42c82ab63759877768d06541748103ac9d40b91a
Lukas Calvo <lu.calvo@duocuc.cl>
2026-05-19 20:01:52 -0400

    refactor(game-catalog): actualizar controller para recibir peticiones de review-service

--- GameCatalog/src/main/java/duoc/cl/GameCatalog/GameCatalogApplication.java
+++ GameCatalog/src/main/java/duoc/cl/GameCatalog/GameCatalogApplication.java
@@ -2,8 +2,10 @@ package duoc.cl.GameCatalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class GameCatalogApplication {
 
 	public static void main(String[] args) {
---
+++ ReviewService/src/main/java/duoc/cl/ReviewService/client/GameCatalogClient.java
@@ -0,0 +1,4 @@
+package duoc.cl.ReviewService.client;
+
+public class GameCatalogClient {
+}
